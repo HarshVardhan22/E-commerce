f9d6732a84f94633a381af39ff1362ef03389d0e
Harshvardhan <hsinha31@gmail.com>
2020-10-20 23:56:57 +0530

    v1

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app =express();
+require('dotenv').config();
+
+app.get("/", (req,res)=>{
+    res.send("Hello from node!");
+})
+
+const port = process.env.PORT||8000;
+
+app.listen()
