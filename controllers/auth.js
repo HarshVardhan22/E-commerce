f5d2c9ef5fd0ad3e66764b2773c5500ef2c6623d
HarshVardhan22 <hsinha31@gmail.com>
2020-11-03 03:37:54 +0530

    added features isAuth and isAdmin

--- controllers/auth.js
+++ controllers/auth.js
@@ -68,3 +68,23 @@ exports.requireSignin = expressJwt({
   userProperty: "auth",
 });
 
+// isAuth function will be used to prevent any user to accessing the deatils of other users
+exports.isAuth = (req,res,next) => {
+  let user = req.profile && req.auth && req.profile._id === req.auth._id;
+  if(!user){
+    return res.status(403).json({
+      error: "Access Denied"
+    });
+  }
+  next();
+}
+
+
+// isAdmin will check if the user is admin or not
+exports.isAdmin = (req,res,next)=>{
+  if(req.profile.role===0){
+    return res.status(403).json({
+      error:"Access Denied"
+    });
+  }
+}
