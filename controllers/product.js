714a3d9e4710d2adb64109c6f3d905b36b3ec0f3
HarshVardhan22 <hsinha31@gmail.com>
2020-11-08 05:39:09 +0530

    fetch products by using sortBy, createdAt,limit

--- controllers/product.js
+++ controllers/product.js
@@ -127,3 +127,30 @@ exports.update = (req,res) => {
 
     })
 };
+
+/**summon products on the basis of sell/arrival */
+//sell = /products?sortBy=sold&order=desc&limit=4
+//the above will show maximum sold products in descending order with a limit of 4per page
+//arrival = /products?sortBy=createdAt&order=desc&limit=4
+//the above will show newly added products in descending order with a limit of 4per page
+
+//however no params are send then all products will be shown
+
+exports.list=(req,res) =>{
+    let order = req.query.order?req.query.order:'asc';
+    let sortBy = req.query.sortBy?req.query.sortBy:'_id';
+    let limit = req.query.limit?parseInt(req.query.limit):7;
+
+    Product.find().select("-photo")
+                  .populate('category')
+                  .sort([[sortBy,order]])
+                  .limit(limit)
+                  .exec((err,products)=>{
+                      if(err){
+                          return res.status(400).json({
+                              error:'Products not found'
+                          })
+                      }
+                      res.send(products)
+                  })
+}
--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const {create, productById, read, remove, update} = require("../controllers/product");
+const {create, productById, read, list, remove, update} = require("../controllers/product");
 
 const { userById} = require("../controllers/user");
 
@@ -12,7 +12,7 @@ router.get("/product/:productId",read)
 router.post("/product/create/:userId",requireSignin,isAuth,isAdmin,create); 
 router.delete("/product/:productId/:userId/",requireSignin,isAuth,isAdmin,remove);
 router.put("/product/:productId/:userId/",requireSignin,isAuth,isAdmin,update);
-
+router.get("/products",list);
 
 router.param("userId", userById);
 router.param("productId", productById);
